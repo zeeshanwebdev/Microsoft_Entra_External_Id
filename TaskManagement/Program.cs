924bc4b7ade0d8bb392e05333468c73f824cb3fc
Muhammad Zeeshan <zeeshan.webdev786@gmail.com>
2025-07-15 03:19:31 +0500

    Handle Post Logout, Pre Logout & OnTokenValidated Events in Entra External Id

--- TaskManagement/Program.cs
+++ TaskManagement/Program.cs
@@ -5,22 +5,38 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using TaskManagement.Context;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<TaskManagementContext>(options =>
-{
-    options.UseInMemoryDatabase("TaskManagements");
-});
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
 
 JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration);
+
+builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.SaveTokens = true;
+
+    options.Events.OnRedirectToIdentityProviderForSignOut += async context =>
+    {
+        await NotifyToExternalAPIs(context.HttpContext);
+    };
+    // Post login call to extend claims and use later
+    options.Events.OnTokenValidated += async context =>
+    {
+        // Extend claims or call service which responsible to extend claims
+        // This only calls when user is login and add claims 
+
+        await ExtenClaimsAsync(context);
+    };
+});
+builder.Services.AddDbContext<TaskManagementContext>(options =>
+{
+    options.UseInMemoryDatabase("TaskManagements");
+});
+
 
 builder.Services.AddControllersWithViews(options =>
 {
@@ -29,6 +45,8 @@ builder.Services.AddControllersWithViews(options =>
         .Build();
     options.Filters.Add(new AuthorizeFilter(policy));
 }).AddMicrosoftIdentityUI();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -56,3 +74,29 @@ app.MapControllerRoute(
 
 app.Run();
 
+
+async Task NotifyToExternalAPIs(HttpContext context)
+{
+    // Actual async work here
+    Console.WriteLine("Performing pre-logout actions");
+    await Task.Delay(1);
+}
+
+async Task ExtenClaimsAsync(TokenValidatedContext context)
+{
+
+    // we can add db logic here or call db service to fetch data and then
+    // can extend claims as i did below.
+
+    // You can now use todoService
+    // Actual async work here
+    if (context.Principal.Identity is ClaimsIdentity identity && identity.IsAuthenticated)
+    {
+        // Add or modify claims here
+        if (!identity.HasClaim(c => c.Type == "CustomClaim"))
+        {
+            identity.AddClaim(new Claim("CustomClaim", "new custom claim"));
+        }
+    }
+    await Task.Delay(1);
+}
